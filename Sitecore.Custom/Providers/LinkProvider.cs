90e7d60d1353eabeb3c6deaaf5d8377e4e84a119
Nishant Agarwal <agarwal.nishant07@gmail.com>
2013-04-28 09:50:25 +0530

    Simplified link provider code

--- Sitecore.Custom/Providers/LinkProvider.cs
+++ Sitecore.Custom/Providers/LinkProvider.cs
@@ -11,11 +11,6 @@ namespace Sitecore.Custom.Providers
     public class LinkProvider : Sitecore.Links.LinkProvider
     {
         /// <summary>
-        /// Gets or sets the list of sites to be excluded from custom link generation.
-        /// </summary>
-        public String ExcludeSites { get; set; }
-
-        /// <summary>
         /// Returns user friendly URL for given Sitecore item.
         /// </summary>
         /// <param name="item">The Sitecore item for which URL is needed.</param>
@@ -23,23 +18,14 @@ namespace Sitecore.Custom.Providers
         /// <returns>User friendly URL for Sitecore item.</returns>
         public override string GetItemUrl(Sitecore.Data.Items.Item item, Sitecore.Links.UrlOptions options)
         {
-            // Check if context site is part of <ExcludeSites> or not.
-            if (Sitecore.Context.Site != null && !ExcludeSites.Contains(Sitecore.Context.Site.Name.ToLower()))
-            {
-                // Get URL from Sitecore out of box link provider.
-                string originalURL = base.GetItemUrl(item, options);
+            // Get URL from Sitecore out of box link provider.
+            string originalURL = base.GetItemUrl(item, options);
 
-                // Get contentStartItem path for context site.
-                string contentStartItem = Sitecore.Context.Site.RootPath.ToLower();
+            // Get contentStartItem path for context site.
+            string contentStartItem = Sitecore.Context.Site.RootPath.ToLower();
 
-                // Replace contentStartItem path in originalURL with empty string and return the resultant string.
-                return originalURL.ToLower().Replace(contentStartItem, String.Empty);
-            }
-            else
-            {
-                // Return the URL from Sitecore out of box link provider.
-                return base.GetItemUrl(item, options); ;
-            }
+            // Replace contentStartItem path in originalURL with empty string and return the resultant string.
+            return originalURL.ToLower().Replace(contentStartItem, String.Empty);
         }
     }
 }
